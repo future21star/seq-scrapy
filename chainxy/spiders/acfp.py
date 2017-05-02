5643d065b1c72c7f0e50d5582cbe90a476a4b415
future21star <future21star@yahoo.com>
2017-05-02 14:54:20 +0800

    update harvey

---
+++ chainxy/spiders/acfp.py
@@ -0,0 +1,67 @@
+import scrapy
+import json
+import csv
+from scrapy.spiders import Spider
+from scrapy.http import FormRequest
+from scrapy.http import Request
+from scrapy.selector import HtmlXPathSelector
+from chainxy.items import ChainItem
+import pdb
+
+class AcfpSpider(scrapy.Spider):
+	name = "acfp"
+	uid_list = []
+	start_urls = ["https://acfp.com/locations/"]
+		
+	def parse(self, response):
+		try:
+			store_urls = response.xpath('//li[contains(@class, "menu-item-object-acfp_location_state")]')
+			for store in store_urls:
+				url = store.xpath('./a/@href').extract_first()	
+				yield scrapy.Request(url = url, callback=self.parse_store)
+		except:
+			pdb.set_trace()
+			pass
+
+	def parse_store(self, response):
+		try:
+			stores = response.xpath('//div[@class="inside"]/div[@class="a-location"]')
+			for store in stores:
+				item = ChainItem()
+				item['store_name'] = self.validate(store.xpath('.//div[@class="name"]/text()'))
+				item['store_number'] = ""
+				item['address'] = store.xpath('.//div[@class="address"]/p/text()').extract_first()
+				item['address2'] = ""
+				item['city'] = store.xpath('.//div[@class="address"]/p/text()').extract()[1].strip().split(',')[0].strip()
+				item['state'] = store.xpath('.//div[@class="address"]/p/text()').extract()[1].strip().split(',')[1].split()[0]
+				item['zip_code'] = store.xpath('.//div[@class="address"]/p/text()').extract()[1].strip().split(',')[1].split()[1]
+				item['phone_number'] = store.xpath('.//a[@class="phone"]/text()').extract_first()
+				item['country'] = "United States"
+				item['latitude'] = store.xpath('.//a[@class="directions"]/@href').extract_first().split('/')[-1][1:-1].split(',')[0]
+				item['longitude'] = store.xpath('.//a[@class="directions"]/@href').extract_first().split('/')[-1][1:-1].split(',')[1]
+				item['store_hours'] = ""
+				if self.validate_with_index(store.xpath('.//div[@class="hours"]//text()'), 4) != "":
+					item['store_hours'] = self.validate_with_index(store.xpath('.//div[@class="hours"]//text()'), 4) + self.validate_with_index(store.xpath('.//div[@class="hours"]//text()'),5) + ";"
+				if self.validate_with_index(store.xpath('.//div[@class="hours"]//text()'),7) != "":
+					item['store_hours'] += self.validate_with_index(store.xpath('.//div[@class="hours"]//text()'),7) + self.validate_with_index(store.xpath('.//div[@class="hours"]//text()'), 8) + ";" 
+				item['other_fields'] = ""
+				item['coming_soon'] = 0
+				if item['phone_number'] == "" or item['phone_number'] in self.uid_list:
+					continue
+				self.uid_list.append(item['phone_number'])
+				yield item
+		except:
+			pdb.set_trace()
+			pass
+
+	def validate(self, xpath):
+		try:
+			return xpath.extract_first().strip()
+		except:
+			return ""
+
+	def validate_with_index(self, xpath, index):
+		try:
+			return xpath.extract()[index]
+		except:
+			return ""
---
+++ chainxy/spiders/harveys.py
@@ -0,0 +1,115 @@
+import scrapy
+import json
+import csv
+from scrapy.spiders import Spider
+from scrapy.http import FormRequest
+from scrapy.http import Request
+from scrapy.selector import HtmlXPathSelector
+from chainxy.items import ChainItem
+import pdb
+
+class HarveysSpider(scrapy.Spider):
+	name = "harveys"
+	uid_list = []
+	start_urls = ["https://www.harveys.ca/eng/locations?province_val=mb&city_val=Winnipeg&imageField.x=226&imageField.y=15"]
+		
+	def parse(self, response):
+		try:
+			markers = response.xpath('//script/text()').extract_first()
+			markers = markers[markers.find("createMarker(new") + 16: -1]
+			pos = 0;
+			while markers.index('location_map.php',pos, -1) > -1:
+				pos = markers.index('location_map.php', pos, -1)
+				url = ""
+				while markers[pos] != "'":
+					url += markers[pos]
+					pos += 1
+				store_number = url[url.find("id=")+3 : -1]
+				item = ChainItem()
+				item['store_number'] = store_number
+				url = url.replace(".php", "")
+				url = "https://www.harveys.ca/eng/" + url
+				request = scrapy.Request(url = url, callback=self.parse_store)
+				request.meta['item'] = item
+				yield request
+		except:
+			pass
+
+	def parse_store(self, response):
+		try:
+			item = response.meta['item']
+			stores = response.xpath('//span[@class="tx_grey_3"]').extract_first()
+			pos = response.body.index('class="tx_grey_3"') + 18 
+			item['address'] = item['city'] = item['state'] = item['zip_code'] = item['store_hours'] = item['phone_number'] = ''
+			while response.body[pos] != ',':
+				item['address'] += response.body[pos]
+				pos += 1
+			item['address'] = self.replaceUnknownLetter(item['address'])
+			pos += 1
+			while response.body[pos] != ',':
+				item['city'] += response.body[pos]
+				pos += 1
+			pos += 1
+			while response.body[pos] != ',':
+				item['state'] += response.body[pos]
+				pos += 1
+			pos += 1
+			while response.body[pos] != '<':
+				item['zip_code'] += response.body[pos]
+				pos += 1
+			item['zip_code'] = item['zip_code'].strip()
+			pos += 1
+			pos = response.body.index('Hours...</strong><br>') + 21
+			while response.body[pos: pos+6] != '<br><s':
+				item['store_hours'] += response.body[pos]
+				pos += 1
+			item['store_hours'] = item['store_hours'].strip()
+			item['store_hours'] = item['store_hours'].replace('<br>', ';')
+			pos = response.body.index("Phone:</strong>") + 15
+			while response.body[pos] != '<':
+				item['phone_number'] += response.body[pos]
+				pos += 1
+			item['phone_number'] = item['phone_number'].strip()
+			
+			pos = response.body.index("setCenter(new GLatLng(") + 22
+			item['latitude'] = ""
+			while response.body[pos] != ',':
+				item['latitude'] += response.body[pos]
+				pos += 1
+			item['latitude'] = item['latitude'].strip()
+			item['longitude'] = ""
+			pos += 1
+			while response.body[pos] != ')':
+				item['longitude'] += response.body[pos]
+				pos += 1
+			item['longitude'] = item['longitude'].strip()
+			item['other_fields'] = ""
+			item['coming_soon'] = 0
+			if item['phone_number'] == "" or item['phone_number'] in self.uid_list:
+				return
+			self.uid_list.append(item['phone_number'])
+			yield item
+		except:
+			pdb.set_trace()
+			pass
+
+	def validate(self, xpath):
+		try:
+			return xpath.extract_first().strip()
+		except:
+			return ""
+
+	def validate_with_index(self, xpath, index):
+		try:
+			return xpath.extract()[index]
+		except:
+			return ""
+
+	def replaceUnknownLetter(self, source):
+		try:
+			formatted_value = source.encode('utf8').replace('\xc3', '').replace('\xa9', 'e').replace('\xa8', 'e').replace('\xb4', 'o').replace('\xb3', 'o').replace('\xb9', 'u').replace('\xba', 'u').replace('\x89', 'E').replace('\xaa', 'e').replace('\x89', 'E').replace('\xa2', 'a').replace('\xac', 'i').replace('\xad', 'i').replace('\xae', 'i')
+			# if "x8" in formatted_value or "x9" in formatted_value or "xa" in formatted_value or "xb" in formatted_value:
+			# 	pdb.set_trace()
+			return formatted_value
+		except:
+			return source
