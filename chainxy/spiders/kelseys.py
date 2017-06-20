26b0090c163a69a31d5559c526409e9cdb1dc6f6
future21star <future21star@yahoo.com>
2017-06-20 23:23:15 +0800

    update kelseys

--- chainxy/spiders/kelseys.py
+++ chainxy/spiders/kelseys.py
@@ -14,67 +14,69 @@ class KelseysSpider(scrapy.Spider):
 	start_urls = ["http://www.kelseys.ca/location_region.php"]
 	count = 0
 	def parse(self, response):
-		# try:
-		states = response.xpath('//select[@id="province"]/option')
-		for state in states:
-			if states.index(state) > 0:
-				state_val = state.xpath('./@value').extract_first()
-				item = ChainItem()
-				item['state'] = state_val
-				url = "http://www.kelseys.ca/location_get_city_loc.php?province=%s" % state_val
-				request = scrapy.Request(url=url, callback=self.parse_state)
-				request.meta['item'] = item
-				yield request
-		# except:
-		# 	pdb.set_trace()
-		# 	pass
+		try:
+			states = response.xpath('//select[@id="province"]/option')
+			for state in states:
+				if states.index(state) > 0:
+					state_val = state.xpath('./@value').extract_first()
+					item = ChainItem()
+					item['state'] = state_val
+					url = "http://www.kelseys.ca/location_get_city_loc.php?province=%s" % state_val
+					request = scrapy.Request(url=url, callback=self.parse_state)
+					request.meta['item'] = item
+					yield request
+		except:
+			pdb.set_trace()
+			pass
 
 	def parse_state(self, response):
-		# try:
-		cities = response.xpath('//select[@class="locatorSelect"]/option')
-		for city in cities:
-			if cities.index(city) > 0:
-				city_value = city.xpath('./@value').extract_first()
-				item = response.meta['item']
-				item['city'] = city_value
-				url = "http://www.kelseys.ca/location_region.php?province=%s&s=1&city_drop=%s" % (item['state'], item['city'])
-				request = scrapy.Request(url=url, callback=self.parse_store)
-				request.meta['item'] = item
-				yield request
-		# except:
-		# 	pdb.set_trace()
-		# 	pass
+		try:
+			cities = response.xpath('//select[@class="locatorSelect"]/option')
+			for city in cities:
+				if cities.index(city) > 0:
+					city_value = city.xpath('./@value').extract_first()
+					item = response.meta['item']
+					item['city'] = city_value
+					url = "http://www.kelseys.ca/location_region.php?province=%s&s=1&city_drop=%s" % (item['state'], item['city'])
+					request = scrapy.Request(url=url, callback=self.parse_store)
+					request.meta['item'] = item
+					yield request
+		except:
+			pdb.set_trace()
+			pass
 
 	def parse_store(self, response):
-		# try:
-		item = response.meta['item']
-		stores = response.xpath('//ul[@id="thumbs"]')[0].xpath('./li')
-		if len(stores) == 0:
-			pdb.set_trace()
+		try:
+			item = response.meta['item']
+			if item['city'] == "Barrie":
+				pdb.set_trace()
+			stores = response.xpath('//ul[@id="thumbs"]')[0].xpath('./li')
+			if len(stores) > 1:
+				pdb.set_trace()
 
-		for store in stores:
-			info = store.xpath('.//div[@class="one"]')[0]
-			item['store_number'] = ""
-			item['store_name'] = ""
-			item['address'] = info.xpath('./span[1]/text()').extract()[1].strip()
-			item['address2'] = ""
-			item['city'] = ",".join(self.validate(info.xpath('./p[1]/text()')).split(',')[:-2]).strip()
-			item['state'] = self.validate(info.xpath('./p[1]/text()')).split(',')[-2].strip()
-			item['zip_code'] = self.validate(info.xpath('./p[1]/text()')).split(',')[-1].strip()
-			item['country'] = "Canada" 
-			item['phone_number'] = self.validate(info.xpath('./p[1]/strong[1]/a[1]/text()')).split(':')[-1].strip()
-			item['store_hours'] = ""
-			item['latitude'] = self.validate(info.xpath('./p[2]/a[1]/img/@src')).split('=')[-1].split(',')[0]
-			item['longitude'] = self.validate(info.xpath('./p[2]/a[1]/img/@src')).split('=')[-1].split(',')[1]
-			#item['store_type'] = info_json["@type"]
-			item['other_fields'] = ""
-			item['coming_soon'] = 0
-			self.count += 1
-			print self.count
-			yield item
-		# except:
-		# 	pdb.set_trace()
-		# 	pass
+			for store in stores:
+				info = store.xpath('.//div[@class="one"]')[0]
+				item['store_number'] = ""
+				item['store_name'] = ""
+				item['address'] = info.xpath('./span[1]/text()').extract()[1].strip()
+				item['address2'] = ""
+				item['city'] = ",".join(self.validate(info.xpath('./p[1]/text()')).split(',')[:-2]).strip()
+				item['state'] = self.validate(info.xpath('./p[1]/text()')).split(',')[-2].strip()
+				item['zip_code'] = self.validate(info.xpath('./p[1]/text()')).split(',')[-1].strip()
+				item['country'] = "Canada" 
+				item['phone_number'] = self.validate(info.xpath('./p[1]/strong[1]/a[1]/text()')).split(':')[-1].strip()
+				item['store_hours'] = ""
+				item['latitude'] = self.validate(info.xpath('./p[2]/a[1]/img/@src')).split('=')[-1].split(',')[0]
+				item['longitude'] = self.validate(info.xpath('./p[2]/a[1]/img/@src')).split('=')[-1].split(',')[1]
+				#item['store_type'] = info_json["@type"]
+				item['other_fields'] = ""
+				item['coming_soon'] = 0
+				self.count += 1
+				print self.count
+				yield item
+		except:
+			pdb.set_trace()
+			pass
 
 	def validate(self, xpath):
 		# try:
---
+++ chainxy/spiders/travel.py
@@ -0,0 +1,91 @@
+import scrapy
+import json
+import csv
+from scrapy.spiders import Spider
+from scrapy.http import FormRequest
+from scrapy.http import Request
+from scrapy.selector import HtmlXPathSelector
+from chainxy.items import ChainItem
+import pdb
+import unicodedata
+
+class TravelSpider(scrapy.Spider):
+		name = "travel"
+		uid_list = []
+		start_urls = ['http://marriotthotelapi.netlinkrg.com/api/hotels?latitude=38.2172124&longitude=-85.42724020000003&max_distance=3000&brand=SHS']
+		count = 0
+
+		def parse(self, response):
+			try:
+				stores = json.loads(response.body)
+				for store in stores:
+					item = ChainItem()
+					item['store_number'] = ""
+					item['store_name'] = store['hotel_name']
+					item['address'] = store['address_line']
+					item['address2'] = ""
+					item['city'] = store['city_name']
+					item['state'] = store['state_prov_code']
+					item['zip_code'] = store['postal_code']
+					item['country'] = "United States" 
+					item['phone_number'] = store['phone']
+					item['store_hours'] = ""
+					item['latitude'] = store['latitude']
+					item['longitude'] = store['longitude']
+					#item['store_type'] = info_json["@type"]
+					item['other_fields'] = ""
+					item['coming_soon'] = 0
+					yield item
+			except:
+				pass			
+
+		def validate(self, xpath):
+			try:
+				return xpath.extract_first().strip()
+			except:
+				return ""
+
+		def isNumber(self, number):
+			count = 0
+			for char in number:
+				if char.isdigit():
+					count += 1
+			return count > 5
+
+		def getInfo(self, response):
+			try:
+				start = response.body.find("location_data.push( ") + 20
+				pos = response.body.find("location_data.push( ") + 20
+				info = ""
+				while response.body[pos] != '}':
+					if response.body[pos:pos+2] != "\'":
+						info += response.body[pos]
+					else:
+						info += "'"
+					pos += 1
+				info += '}'
+				return info.decode('utf8').replace("\'", "").replace('\t','').replace('\n','')
+			except:
+				return ""
+
+		def getValue(self, info, property):
+			try:
+				pos = info.find(property + ":") + len(property) + 2
+				value = ""
+				while info[pos] != ",":
+					value += info[pos]
+					pos += 1
+				return value
+			except:
+				return ""
+
+		def getSpecificValue(self, info, property):
+			try:
+				pos = info.find(property) + len(property) + 3
+				value = ""
+				while info[pos:pos+2] != "</":
+					value += info[pos]
+					pos += 1
+				return value.strip()
+			except:
+				return ""
